c7616083d79b2d6b24049b9b5ef84c815ed951fc
pankaj <pankajgiri@clear-launch.com>
2017-05-31 01:31:17 -0500

    Initial test commit

---
+++ Database1/Database1/dbo/Tables/Students.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [StudentId] INT           NOT NULL,
+    [FirstName] VARCHAR (35)  NOT NULL,
+    [LastName]  VARCHAR (35)  NOT NULL,
+    [Address]   VARCHAR (100) NULL,
+    [City]      VARCHAR (20)  NULL,
+    [State]     CHAR (2)      NULL,
+    [ZIP]       CHAR (5)      NULL,
+    PRIMARY KEY CLUSTERED ([StudentId] ASC)
+);
+
